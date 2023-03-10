bc4d8e8cb9e194791f3e9ad0651faefe72a9d33b
Kashif Jilani <kashifjilani@Kashifs-MacBook-Pro.local>
2023-03-10 16:42:31 +0530

    Adding cookiecutter template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='End to End Machine Learning Pipeline with MLOps tools',
+    author='kashif jilani',
+    license='MIT',
+)
