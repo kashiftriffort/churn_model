b06e0866fb6bcee8c610beaf91e0042641440009
Kashif Jilani <kashifjilani@Kashifs-MacBook-Pro.local>
2023-03-11 09:03:42 +0530

    load model training, split data, mlflow

---
+++ src/models/train_model.py
@@ -0,0 +1,89 @@
+import json
+import yaml
+import joblib
+import mlflow
+import argparse
+import numpy as np
+import pandas as pd
+from urllib.parse import urlparse
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import f1_score, recall_score, accuracy_score, precision_score, confusion_matrix, classification_report
+
+def read_params(config_path):
+    with open(config_path) as yaml_file:
+        config = yaml.safe_load(yaml_file)
+    return config
+
+def train_and_evaluate(config_path):
+    config = read_params(config_path)
+    train_data_path = config["processed_data_config"]["train_data_csv"]
+    test_data_path = config["processed_data_config"]["test_data_csv"]
+
+    target = config["raw_data_config"]["target"]
+    max_depth = config["random_forest"]["max_depth"]
+    n_estimators = config["random_forest"]["n_estimators"]
+
+    train = pd.read_csv(train_data_path, sep=",")
+    test = pd.read_csv(test_data_path, sep=",")
+    train_x, train_y = get_feat_and_target(train, target)
+    test_x, test_y = get_feat_and_target(test, target)
+
+    mlflow_config = config["mlflow_config"]
+    remote_server_uri = mlflow_config["remote_server_uri"]
+
+    mlflow.set_tracking_uri(remote_server_uri)
+    mlflow.set_experiment(mlflow_config["experiment_name"])
+
+    with mlflow.start_run(run_name = mlflow_config["run_name"]) as mlops_run:
+        model = RandomForestClassifier(max_depth=max_depth, n_estimators=n_estimators)
+        model.fit(train_x, train_y)
+        y_pred = model.predict(test_x)
+        accuracy, precision, recall, f1score = accuracymeasures(test_y, y_pred, 'weighted')
+
+        mlflow.log_param("max_depth", max_depth)
+        mlflow.log_param("n_estimators", n_estimators)
+
+        mlflow.log_metric("accuracy", accuracy)
+        mlflow.log_metric("precision", precision)
+        mlflow.log_metric("recall", recall)
+        mlflow.log_metric("f1_score", f1score)
+
+        tracking_url_type_store = urlparse(mlflow.get_artifact_uri()).scheme
+        if tracking_url_type_store != "file":
+            mlflow.sklearn.log_model(model, "model", registered_model_name=mlflow_config["registered_model_name"])
+        else:
+            mlflow.sklearn.load_model(model, "model")
+
+
+def get_feat_and_target(df, target):
+    X = df.drop(target, axis=1)
+    y = df[target]
+    return X, y
+
+def accuracymeasures(y_test,predictions,avg_method):
+    accuracy = accuracy_score(y_test, predictions)
+    precision = precision_score(y_test, predictions, average=avg_method)
+    recall = recall_score(y_test, predictions, average=avg_method)
+    f1score = f1_score(y_test, predictions, average=avg_method)
+    target_names = ['0','1']
+    print("Classification report")
+    print("---------------------","\n")
+    print(classification_report(y_test, predictions,target_names=target_names),"\n")
+    print("Confusion Matrix")
+    print("---------------------","\n")
+    print(confusion_matrix(y_test, predictions),"\n")
+
+    print("Accuracy Measures")
+    print("---------------------","\n")
+    print("Accuracy: ", accuracy)
+    print("Precision: ", precision)
+    print("Recall: ", recall)
+    print("F1 Score: ", f1score)
+
+    return accuracy,precision,recall,f1score
+
+if __name__=="__main__":
+    args = argparse.ArgumentParser()
+    args.add_argument("--config", default="params.yaml")
+    parsed_args = args.parse_args()
+    train_and_evaluate(config_path=parsed_args.config)
